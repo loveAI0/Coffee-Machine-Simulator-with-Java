3efaffa72675189bf8308f226a50f501f70da805
loveAI0 <140394889+loveAI0@users.noreply.github.com>
2025-05-20 19:52:22 +0000

    step 3  under process.

--- Main.java
+++ Main.java
@@ -1,8 +1,9 @@
 import java.util.Scanner;
 public class Main{
     public static void main(String[] args){
-        consoleOutput();
-        requirementAnalysis();
+       // consoleOutput();
+        // requirementAnalysis();
+        estimationOfServing();
     }
     static void consoleOutput(){
         String consoleOutput = """
@@ -26,9 +27,49 @@ public class Main{
         System.out.println(String.format("%d ml of water", 200 * coffeeCup));
         System.out.println(String.format("%d ml of milk", 50 * coffeeCup));
         System.out.println(String.format("%d g of coffee beans", 15 * coffeeCup));
+    }
 
+    static void estimationOfServing(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Write how many ml of water the coffee machine has:");
+        int waterQuantity = input.nextInt();
+        double waterAvailable = (double) waterQuantity / 200 ; // for cups of coffee
+
+        System.out.println("Write how many ml of milk the coffee machine has:");
+        int milkQuantity = input.nextInt();
+        double milkAvailable = (double) milkQuantity / 50 ; // for cups of coffee
+
+        System.out.println("Write how many grams of coffee beans the coffee machine has:");
+        int beansQuantity = input.nextInt();
+        double beansAvailable = (double) beansQuantity / 15 ; // for cups of coffee
+
+        System.out.println("Write how many cups of coffee you will need:");
+        double coffeeNeed = input.nextDouble();
+        double coffeeAvailable = 0;
+       
+        if(waterAvailable < milkAvailable){
+            if(waterAvailable < beansAvailable){
+                coffeeAvailable = waterAvailable;
+            }
+            else{
+                coffeeAvailable = beansAvailable;
+            }
+        }else{
+            coffeeAvailable = milkAvailable;
+        }
         
 
+        if(coffeeAvailable == coffeeNeed){
+            System.out.println("Yes, I can make that amount of coffee");
+        }
+        else if(coffeeAvailable > coffeeNeed){
+            System.out.println(String.format("Yes, I can make that amount of coffee (and even %.0f more than that)", coffeeAvailable - coffeeNeed));
+        }
 
+        else{
+            System.out.println(String.format("No, I can make only %.0f cup(s) of coffee", coffeeAvailable));
+        }
+        
+        
     }
 }
