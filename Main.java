4359ecb106b237cae3e93b79f41259e55426d690
loveAI0 <140394889+loveAI0@users.noreply.github.com>
2025-05-20 17:53:57 +0000

    console out of process

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main{
+    public static void main(String[] args){
+        String consoleOutput = """
+                Starting to make a coffee
+                Grinding coffee beans
+                Boiling water
+                Mixing boiled water with crushed coffee beans
+                Pouring coffee into the cup
+                Pouring some milk into the cup
+                Coffee is ready!""";
+        System.out.println(consoleOutput);
+    }
+}
