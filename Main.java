c828cf60e4d9842233cffc1dad05dfc9d98e02d4
loveAI0 <140394889+loveAI0@users.noreply.github.com>
2025-05-21 08:31:07 +0000

    RequiremetServing is analyzed

--- Main.java
+++ Main.java
@@ -34,14 +34,17 @@ public class Main{
         System.out.println("Write how many ml of water the coffee machine has:");
         int waterQuantity = input.nextInt();
         double waterAvailable = (double) waterQuantity / 200 ; // for cups of coffee
+        System.out.println(waterAvailable);
 
         System.out.println("Write how many ml of milk the coffee machine has:");
         int milkQuantity = input.nextInt();
         double milkAvailable = (double) milkQuantity / 50 ; // for cups of coffee
+        System.out.println(milkAvailable);
 
         System.out.println("Write how many grams of coffee beans the coffee machine has:");
         int beansQuantity = input.nextInt();
         double beansAvailable = (double) beansQuantity / 15 ; // for cups of coffee
+        System.out.println(beansAvailable);
 
         System.out.println("Write how many cups of coffee you will need:");
         double coffeeNeed = input.nextDouble();
@@ -59,15 +62,15 @@ public class Main{
         }
         
 
-        if(coffeeAvailable == coffeeNeed){
+        if(Math.floor(coffeeAvailable) == Math.floor(coffeeNeed)){
             System.out.println("Yes, I can make that amount of coffee");
         }
-        else if(coffeeAvailable > coffeeNeed){
-            System.out.println(String.format("Yes, I can make that amount of coffee (and even %.0f more than that)", coffeeAvailable - coffeeNeed));
+        else if(Math.floor(coffeeAvailable) > Math.floor(coffeeNeed)){
+            System.out.println(String.format("Yes, I can make that amount of coffee (and even %.0f more than that)", Math.floor(coffeeAvailable) - Math.floor(coffeeNeed)));
         }
 
         else{
-            System.out.println(String.format("No, I can make only %.0f cup(s) of coffee", coffeeAvailable));
+            System.out.println(String.format("No, I can make only %.0f cup(s) of coffee", Math.floor(coffeeAvailable)));
         }
         
         
